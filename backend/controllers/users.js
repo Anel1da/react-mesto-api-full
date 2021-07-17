031f140fd434b685a8e9b93b354d71485e3a5348
Lidya <anel1da@yandex.ru>
2021-07-18 01:15:42 +0300

    test исправила env

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,6 +2,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/users");
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const BadRequestError = require("../errors/bad-request-err"); // 400
 const UnauthorizedError = require("../errors/unauthorized-err"); // 401
 const NotFoundError = require("../errors/not-found-err"); // 404
@@ -39,9 +41,13 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, "some-secret-key", {
-        expiresIn: "7d",
-      });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+        {
+          expiresIn: "7d",
+        }
+      );
       res
         .cookie("jwt", token, {
           maxAge: 3600000 * 24 * 7,
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,12 +1,17 @@
 const jwt = require("jsonwebtoken");
 const UnauthorizedError = require("../errors/unauthorized-err"); // 401
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(token, "some-secret-key");
+    payload = jwt.verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET : "dev-secret"
+    );
   } catch (err) {
     throw new UnauthorizedError("Необходима авторизация");
   }
