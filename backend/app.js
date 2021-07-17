3d5a8d97c156a2db2311eee59a42849f0c45ba8d
Lidya <anel1da@yandex.ru>
2021-07-17 18:21:47 +0300

    readme+crash test

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
 
+// краш-тест сервера
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
+
 // роуты регистрации и авторизации
 app.post(
   "/signin",
